fa6c3352a6e30b2690e99bceaa1ce62606ed4b55
Martin Löper <martin.loeper@nesto-software.de>
2022-08-11 20:04:34 +0000

    fix: networking and setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,4 +7,7 @@ echo "Installing craft using composer..."
 
 # see: https://craftcms.com/docs/3.x/installation.html#step-2-set-the-file-permissions
 echo "Setting correct permissions..."
-(cd "$DIR/.." && chmod -Rf 0777 .env composer.json composer.lock config/license.key config/project storage vendor web/cpresources/)
+(cd "$DIR/.." && sudo chmod -Rf 0777 .env composer.json composer.lock config/license.key config/project storage/* vendor web/cpresources/)
+
+echo "Run the craft setup..."
+(cd "$DIR/.." && php craft setup)
