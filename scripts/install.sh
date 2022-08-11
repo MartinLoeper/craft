4692206d257d9b511f1f90e67cebec386d705adf
Martin Löper <martin.loeper@nesto-software.de>
2022-08-11 19:23:34 +0000

    feat: add docker-compose setup

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Installing craft using composer..."
+(cd "$DIR/.." && composer install)
+
+# see: https://craftcms.com/docs/3.x/installation.html#step-2-set-the-file-permissions
+echo "Setting correct permissions..."
+(cd "$DIR/.." && chmod -Rf 0777 .env composer.json composer.lock config/license.key config/project storage vendor web/cpresources/)
