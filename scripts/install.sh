7132732b9ec3183255be77fe9313a256e890dfe3
Martin Löper <martin.loeper@nesto-software.de>
2022-08-11 20:21:48 +0000

    fix: installer script and env

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,9 @@ echo "Installing craft using composer..."
 
 # see: https://craftcms.com/docs/3.x/installation.html#step-2-set-the-file-permissions
 echo "Setting correct permissions..."
-(cd "$DIR/.." && sudo chmod -Rf 0777 .env composer.json composer.lock config/license.key config/project storage/* vendor web/cpresources/)
+(cd "$DIR/.." && sudo chmod -Rf 0777 .env composer.json composer.lock config/* config storage/* vendor web/cpresources/)
 
 echo "Run the craft setup..."
 (cd "$DIR/.." && php craft setup)
+
+echo "Now you can run docker-compose up!"
